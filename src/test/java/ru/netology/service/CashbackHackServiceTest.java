49600dd262a118867363f29d059f4f05c1afa21a
Ondine <ondinevika@gmail.com>
2023-06-23 13:08:29 +0300

    change build

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,33 +1,36 @@
 package ru.netology.service;
 
+
 import static org.testng.Assert.assertEquals;
 
-
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @org.testng.annotations.Test
-    public void shouldRemainTheAmount(){
+    public void shouldRemainTheAmount() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
+
     @org.testng.annotations.Test
-    public void shouldCalculateWith900(){
+    public void shouldCalculateWith900() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
+
     @org.testng.annotations.Test
-    public void shouldCalculateLessThen1000(){
+    public void shouldCalculateLessThen1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 500;
         int actual = service.remain(amount);
         int expected = 500;
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 
 }
+
